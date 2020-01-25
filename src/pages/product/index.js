39ee729502a2daa0bb375bb6d6544fa3ac14c68c
Lucas Gomes <lucasgms16@gmail.com>
2020-01-25 14:14:11 -0300

    ez pl lmn sqz

---
+++ src/pages/product/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import './styles.css';
+
+const Product = (props) => {
+
+	const [product, setProduct] = useState({});
+
+	useEffect(() => {
+		findProduct();
+	}, [])
+	
+
+	const findProduct = async () => {
+
+		const { id } = props.match.params;
+		const response = await api.get(`/products/${id}`)
+		setProduct(response.data);
+
+	};
+	return (
+		<div className="product-info">
+			<h1>{product.title}</h1>
+			<p>{product.description}</p>
+
+			<p>
+				URL: <a href={product.url}>{product.url}</a>
+			</p>
+		</div>
+
+	)
+}
+
+export default Product;
